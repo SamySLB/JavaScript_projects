fe40c1243929b49c104e63c477c72f34c9d587b7
SAMYSBL <samara.barcelos@live.com>
2026-02-13 15:00:18 -0300

    conexão com back

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,21 @@
+import { useEffect } from "react";
+import api from "./api/api";
+
 function App() {
+
+  useEffect(() => {
+    api.get("/pedidos/meus")
+      .then(res => {
+        console.log("Resposta do backend:", res.data);
+      })
+      .catch(err => {
+        console.error("Erro:", err);
+      });
+  }, []);
+
   return (
     <div className="container mt-5">
-      <h1 className="text-primary">Projeto Loja</h1>
-      <button className="btn btn-success">Botão Teste</button>
+      <h1 className="text-primary">Integração Backend OK?</h1>
     </div>
   );
 }
