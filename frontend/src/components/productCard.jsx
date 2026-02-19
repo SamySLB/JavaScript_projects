be4860a2e6f28e15dd7bcf6021f67ac73b39935f
SAMYSBL <samara.barcelos@live.com>
2026-02-19 17:57:15 -0300

    cards de produtos implementados na home com api de roupas

--- frontend/src/components/productCard.jsx
+++ frontend/src/components/productCard.jsx
@@ -2,16 +2,26 @@ function ProductCard({ nome, preco, imagem }) {
   return (
     <div className="col-md-3 mb-4">
       <div className="card h-100 shadow-sm">
-        <img
-          src={imagem}
-          className="card-img-top"
-          alt={nome}
-        />
+
+        <div style={{ height: "300px", overflow: "hidden" }}>
+          <img
+            src={imagem}
+            alt={nome}
+            style={{
+              width: "100%",
+              height: "100%",
+              objectFit: "contain"
+            }}
+          />
+        </div>
 
         <div className="card-body text-center">
-          <h6>{nome}</h6>
-          <p className="fw-bold">R$ {preco}</p>
+          <h6 className="text-truncate">{nome}</h6>
+          <p className="fw-bold">
+            R$ {preco.toFixed(2)}
+          </p>
         </div>
+
       </div>
     </div>
   );
