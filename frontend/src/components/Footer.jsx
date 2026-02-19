b829bb69717a5ade1d6e7c0ff400aaba552dc562
SAMYSBL <samara.barcelos@live.com>
2026-02-19 16:54:49 -0300

    criando footer

---
+++ frontend/src/components/Footer.jsx
@@ -0,0 +1,11 @@
+function Footer() {
+  return (
+    <footer className="bg-dark text-white text-center py-4 mt-5">
+      <p className="mb-1">Endereço: Rua Tavarez, 123</p>
+      <p className="mb-1">Telefone: (11) 99999-9999</p>
+      <p>Email: contato@modastore.com</p>
+    </footer>
+  );
+}
+
+export default Footer;
