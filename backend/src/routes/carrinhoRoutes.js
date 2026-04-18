3b771f2ea51a9e8524288d37dc6682b6e1a1e9a6
SAMYSBL <samara.barcelos@live.com>
2026-04-18 15:58:24 -0300

    componentizando bt-log-carrin-favoritos

--- backend/src/routes/carrinhoRoutes.js
+++
@@ -1,9 +0,0 @@
-import express from "express";
-import { adicionarCarrinho, listarCarrinho } from "../controllers/CarrinhoController.js";
-
-const router = express.Router();
-
-router.get("/", listarCarrinho);
-router.post("/", adicionarCarrinho);
-
-export default router;
